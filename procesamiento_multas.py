5a8e88a33955ffa2a4172c175563a5b568ea8b6f
Livingston <livin.arias88@gmail.com>
2022-07-05 08:06:05 -0500

    agrego modificaciones para procesar los datos y reconocer tipo de vehiculo

--- procesamiento_multas.py
+++ procesamiento_multas.py
@@ -1,11 +1,31 @@
+import pandas
 import consumo_api
-from pandas import DataFrame as pd
+import pandas as pd
 
-dbname = consumo_api.ConsumoHistorialMultas.get_database()
-collection_name = dbname["multas_2000_al_2022"]
-item_details = collection_name.find({"vigencia" : "2006"})
-items_df = pd(item_details)
-print(items_df)
+
+class ProcesamientoData:
+
+    def _update_car_or_motorbike(ciudades):
+        dbname = consumo_api.ConsumoHistorialMultas.get_database()
+        collection_name = dbname["multas_2000_al_2022"]
+        for rec in ciudades:
+            data = collection_name.find({"ciudad": rec})
+            for x in data:
+                if x["placa"] and x["placa"][-1].isnumeric():
+                    newdata = {"$set":{"tipo_vehiculo":"Carro"}}
+                else:
+                    newdata = {"$set":{"tipo_vehiculo":"Moto"}}
+                collection_name.update_one(x, newdata)
+                print("dato modificado ",x["_id"])
+
+    def open_lat_long():
+        df = pd.read_excel('../ciudades.xlsx')
+        ciudades = df["ciudades"]
+        return ciudades
+
+
+test = ProcesamientoData.open_lat_long()
+ProcesamientoData._update_car_or_motorbike(test)
 
 """
 Consumo con power bi
